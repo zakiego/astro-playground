bbe187fc7fc44e471b76a887400b73f5f6537da8
zakiego <belummikir@gmail.com>
2023-07-07 15:29:58 +0800

    mvp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import pandacss from "@pandacss/dev/astro";
+import sitemap from "@astrojs/sitemap";
+import { SITE_CONFIG } from "./src/lib/consts";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [pandacss(), sitemap({})],
+  site: SITE_CONFIG.url,
+});
---
+++ site.config.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+const SITE_CONFIG = {
+  title: "My personal website.",
+  description: "Welcome to my website!",
+  url: "https://www.example.com",
+  sitemap: "/sitemap-index.xml",
+};
+
+export { SITE_CONFIG };
+export default SITE_CONFIG;
