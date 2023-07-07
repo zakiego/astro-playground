dc5a5ae55560561d95f838ae1a767d6a34b0efd1
zakiego <belummikir@gmail.com>
2023-07-07 15:37:50 +0800

    fix: use absolute url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import pandacss from "@pandacss/dev/astro";
 import sitemap from "@astrojs/sitemap";
-import { SITE_CONFIG } from "./src/lib/consts";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [pandacss(), sitemap({})],
-  site: SITE_CONFIG.url,
+  site: "https://www.astro.build",
 });
